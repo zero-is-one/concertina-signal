988e6927e1843fd758f5ecbd7b0d459e0ece76cf
ryohey <info@codingcafe.jp>
2024-04-04 09:59:23 +0900

    Fix forge config

--- electron/forge.config.js
+++ electron/forge.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   packagerConfig: {
+    ignore: [
+      "^/src",
+      "^/README.md",
+      "^/tsconfig.json",
+      "node_modules/electron$",
+      "node_modules/@electron-forge$",
+    ],
+    prune: false,
     extendInfo: {
       CFBundleDocumentTypes: [
         {
