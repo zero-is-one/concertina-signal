810d590fdcefd55eca9813148572483515afcc2e
ryohey <info@codingcafe.jp>
2024-04-19 09:58:02 +0900

    Add useSongFile

--- src/main/components/Navigation/FileMenu.tsx
+++ src/main/components/Navigation/FileMenu.tsx
@@ -1,57 +1,38 @@
 import { observer } from "mobx-react-lite"
 import { FC } from "react"
-import { useLocalization } from "../../../common/localize/useLocalization"
 import { Localized } from "../../../components/Localized"
 import { MenuDivider, MenuItem } from "../../../components/Menu"
-import { createSong, saveSong } from "../../actions"
-import { openFile, saveFile, saveFileAs } from "../../actions/file"
+import { useSongFile } from "../../hooks/useSongFile"
 import { useStores } from "../../hooks/useStores"
-import { useToast } from "../../hooks/useToast"
 
 export const FileMenu: FC<{ close: () => void }> = observer(({ close }) => {
   const rootStore = useStores()
-  const toast = useToast()
-  const localized = useLocalization()
+  const { createNewSong, openSong, saveSong, saveAsSong, downloadSong } =
+    useSongFile()
 
-  const onClickNew = () => {
-    const { song } = rootStore
+  const onClickNew = async () => {
     close()
-    if (
-      song.isSaved ||
-      confirm(localized("confirm-new", "Are you sure you want to continue?"))
-    ) {
-      createSong(rootStore)()
-    }
+    await createNewSong()
   }
 
   const onClickOpen = async () => {
-    const { song } = rootStore
     close()
-    try {
-      if (
-        song.isSaved ||
-        confirm(localized("confirm-open", "Are you sure you want to continue?"))
-      ) {
-        await openFile(rootStore)
-      }
-    } catch (e) {
-      toast.error((e as Error).message)
-    }
+    await openSong()
   }
 
   const onClickSave = async () => {
     close()
-    await saveFile(rootStore)
+    await saveSong()
   }
 
   const onClickSaveAs = async () => {
     close()
-    await saveFileAs(rootStore)
+    await saveAsSong()
   }
 
-  const onClickDownload = () => {
+  const onClickDownload = async () => {
     close()
-    saveSong(rootStore)()
+    await downloadSong()
   }
 
   return (
