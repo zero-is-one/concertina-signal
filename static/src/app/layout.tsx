96c8a9f1e5a1817581c5d63408c76f20fe614c72
ryohey <info@codingcafe.jp>
2024-04-28 11:54:32 +0900

    Add favicon/google analytics

--- static/src/app/layout.tsx
+++ static/src/app/layout.tsx
@@ -1,9 +1,12 @@
+import { GoogleAnalytics } from "@next/third-parties/google"
 import { Inter } from "next/font/google"
+import Head from "next/head"
 import { Provider } from "./Provider"
 import "./carbon.css"
 import "./globals.css"
 
 const inter = Inter({ subsets: ["latin"] })
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -12,6 +15,41 @@ export default function RootLayout({
   return (
     <Provider>
       <html lang="en">
+        <GoogleAnalytics gaId="G-C4N96XS293" />
+        <Head>
+          <link rel="canonical" href="https://signal.vercel.app/" />
+          <link
+            rel="alternate"
+            hrefLang="x-default"
+            href="https://signal.vercel.app/"
+          />
+          <link
+            rel="alternate"
+            hrefLang="en"
+            href="https://signal.vercel.app/?lang=en"
+          />
+          <link
+            rel="alternate"
+            hrefLang="ja"
+            href="https://signal.vercel.app/?lang=ja"
+          />
+          <link
+            rel="alternate"
+            hrefLang="zh"
+            href="https://signal.vercel.app/?lang=zh"
+          />
+          <link
+            rel="alternate"
+            hrefLang="zh-cmn-Hans"
+            href="https://signal.vercel.app/?lang=zh-Hans"
+          />
+          <link
+            rel="alternate"
+            hrefLang="zh-cmn-Hant"
+            href="https://signal.vercel.app/?lang=zh-Hant"
+          />
+          <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        </Head>
         <body className={inter.className}>{children}</body>
       </html>
     </Provider>
