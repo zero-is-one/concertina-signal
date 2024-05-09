1909845e0e5da560605e7dd29f580a5ca16cb911
ryohey <info@codingcafe.jp>
2024-05-09 10:45:16 +0900

    Unbundle react

--- packages/dialog-hooks/vite.config.js
+++ packages/dialog-hooks/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
       formats: ["es"],
     },
     rollupOptions: {
-      external: ["react", "react-dom"],
+      external: ["react", "react-dom", "react/jsx-runtime"],
     },
   },
   plugins: [dts({ rollupTypes: true })],
