e9d84e325eaeade648bd4f154bf7d7320a6a41da
ryohey <info@codingcafe.jp>
2024-06-02 09:59:28 +0900

    chore: Update Localized component to use useLayoutEffect hook

--- static/src/components/Localized.tsx
+++ static/src/components/Localized.tsx
@@ -1,13 +1,13 @@
 "use client"
 
-import { useEffect, useState } from "react"
+import { useLayoutEffect, useState } from "react"
 import localization from "../l18n/localization"
 import type { LocalizationKey } from "../l18n/useLocalization"
 
 export default function Localized({ name }: { name: LocalizationKey }) {
   const [text, setText] = useState<React.ReactNode>(localization.en[name])
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     import("../l18n/useLocalization").then(({ LocalizedInternal }) => {
       setText(<LocalizedInternal name={name} />)
     })
