0a0cbf59c76b48cee44be003e80f58290d06b534
ryohey <info@codingcafe.jp>
2024-06-02 10:20:06 +0900

    css-in-js: Support page

--- static/src/app/support/page.tsx
+++ static/src/app/support/page.tsx
@@ -1,73 +1,99 @@
+import { styled } from "@kuma-ui/core"
 import { Metadata } from "next"
 import { Footer } from "../../components/Footer/Footer"
 import Localized from "../../components/Localized"
 import { Navigation } from "../../components/Navigation/Navigation"
-import styles from "./styles.module.css"
 
 export const metadata: Metadata = {
   title: "Support | signal",
 }
 
+const Content = styled.div`
+  max-width: 60rem;
+  width: 100%;
+  margin: 0 auto;
+`
+
+const Title = styled.h1`
+  margin-top: 4rem;
+`
+
+const Section = styled.section`
+  margin: 3rem 0;
+`
+
+const SectionContent = styled.div`
+  margin-top: 1rem;
+`
+
+const OpenButton = styled.a`
+  display: block;
+  background: var(--themeColor);
+  color: white;
+  padding: 0.5rem 1rem;
+  border-radius: 9999px;
+  text-decoration: none;
+  font-weight: bold;
+  display: inline-block;
+  font-size: 1rem;
+  border: 2px solid transparent;
+
+  &:hover {
+    opacity: 0.9;
+  }
+`
+
 export default function Page() {
   return (
     <>
       <Navigation />
-      <div className={styles.content}>
-        <h1 className={styles.title}>
+      <Content>
+        <Title>
           <Localized name="support" />
-        </h1>
+        </Title>
 
-        <section className={styles.section}>
+        <Section>
           <h2>
             <Localized name="bug-report" />
           </h2>
-          <div className={styles.sectionContent}>
+          <SectionContent>
             <p>
               <Localized name="bug-report-description" />
             </p>
-            <a
-              href="https://github.com/ryohey/signal/issues"
-              className={styles.openButton}
-            >
+            <OpenButton href="https://github.com/ryohey/signal/issues">
               <Localized name="open-github-issues" />
-            </a>
-          </div>
-        </section>
+            </OpenButton>
+          </SectionContent>
+        </Section>
 
-        <section className={styles.section}>
+        <Section>
           <h2>
             <Localized name="community" />
           </h2>
-          <div className={styles.sectionContent}>
+          <SectionContent>
             <p>
               <Localized name="community-description" />
             </p>
-            <a
-              href="https://discord.com/invite/XQxzNdDJse"
-              className={styles.openButton}
-            >
+            <OpenButton href="https://discord.com/invite/XQxzNdDJse">
               <Localized name="join-discord" />
-            </a>
-          </div>
-        </section>
+            </OpenButton>
+          </SectionContent>
+        </Section>
 
-        <section className={styles.section}>
+        <Section>
           <h2>
             <Localized name="twitter" />
           </h2>
-          <div className={styles.sectionContent}>
+          <SectionContent>
             <p>
               <Localized name="follow-twitter" />
             </p>
-            <a
-              href="https://twitter.com/signalmidi"
-              className={styles.openButton}
-            >
+            <OpenButton href="https://twitter.com/signalmidi">
               @signalmidi
-            </a>
-          </div>
-        </section>
-      </div>
+            </OpenButton>
+          </SectionContent>
+        </Section>
+      </Content>
 
       <Footer />
     </>
