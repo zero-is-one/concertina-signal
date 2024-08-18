239cd9095e5d2a587dcaebccd9223d3fa29029b7
ryohey <info@codingcafe.jp>
2024-08-18 16:46:18 +0900

    Fix title

--- app/src/components/Head/Head.tsx
+++ app/src/components/Head/Head.tsx
@@ -2,6 +2,7 @@ import { observer } from "mobx-react-lite"
 import { FC } from "react"
 import { Helmet } from "react-helmet-async"
 import { useStores } from "../../hooks/useStores"
+import Song from "../../song"
 
 export const Head: FC = observer(() => {
   const { song } = useStores()
@@ -9,9 +10,20 @@ export const Head: FC = observer(() => {
   return (
     <Helmet>
       <title>
-        {song.name.length === 0 ? "New song" : song.name}
+        {getSongDisplayName(song)}
         {song.isSaved ? "" : " *"}
+        {" - signal"}
       </title>
     </Helmet>
   )
 })
+
+function getSongDisplayName(song: Song): string {
+  if (song.filepath.length > 0) {
+    return song.filepath
+  }
+  if (song.name.length > 0) {
+    return song.name
+  }
+  return "New song"
+}
