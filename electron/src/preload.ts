1e3da07d77007ec51b0036d9a860700d0adaf4c8
ryohey <info@codingcafe.jp>
2024-08-18 11:24:08 +0900

    refactor: currying register/invoke

--- electron/src/preload.ts
+++ electron/src/preload.ts
@@ -5,60 +5,56 @@ import type { IpcMainAPI } from "./ipcMain"
 
 type Tail<T extends unknown[]> = T extends [any, ...infer Rest] ? Rest : []
 
-function register<T extends IpcEvent["name"]>(
-  name: T,
-  callback: (params: ParamsForEvent<T>) => void,
-) {
-  const listener = (
-    _event: Electron.IpcRendererEvent,
-    value: ParamsForEvent<T>,
-  ) => {
-    callback(value)
+const register =
+  <T extends IpcEvent["name"]>(name: T) =>
+  (callback: (params: ParamsForEvent<T>) => void) => {
+    const listener = (
+      _event: Electron.IpcRendererEvent,
+      value: ParamsForEvent<T>,
+    ) => {
+      callback(value)
+    }
+    ipcRenderer.on(name, listener)
+    return () => {
+      ipcRenderer.removeListener(name, listener)
+    }
   }
-  ipcRenderer.on(name, listener)
-  return () => {
-    ipcRenderer.removeListener(name, listener)
-  }
-}
 
-function invoke<T extends keyof IpcMainAPI>(
-  name: T,
-  ...params: Tail<Parameters<IpcMainAPI[T]>>
-) {
-  return ipcRenderer.invoke(name, ...params) as ReturnType<IpcMainAPI[T]>
-}
+const invoke =
+  <T extends keyof IpcMainAPI>(name: T) =>
+  (...params: Tail<Parameters<IpcMainAPI[T]>>) =>
+    ipcRenderer.invoke(name, ...params) as ReturnType<IpcMainAPI[T]>
 
 const api: ElectronAPI = {
-  onNewFile: (callback) => register("onNewFile", callback),
-  onClickOpenFile: (callback) => register("onClickOpenFile", callback),
-  onOpenFile: (callback) => register("onOpenFile", callback),
-  onSaveFile: (callback) => register("onSaveFile", callback),
-  onSaveFileAs: (callback) => register("onSaveFileAs", callback),
-  onRename: (callback) => register("onRename", callback),
-  onImport: (callback) => register("onImport", callback),
-  onExportWav: (callback) => register("onExportWav", callback),
-  onUndo: (callback) => register("onUndo", callback),
-  onRedo: (callback) => register("onRedo", callback),
-  onCut: (callback) => register("onCut", callback),
-  onCopy: (callback) => register("onCopy", callback),
-  onPaste: (callback) => register("onPaste", callback),
-  onOpenSetting: (callback) => register("onOpenSetting", callback),
-  onOpenHelp: (callback) => register("onOpenHelp", callback),
-  onBrowserSignInCompleted: (callback) =>
-    register("onBrowserSignInCompleted", callback),
+  onNewFile: register("onNewFile"),
+  onClickOpenFile: register("onClickOpenFile"),
+  onOpenFile: register("onOpenFile"),
+  onSaveFile: register("onSaveFile"),
+  onSaveFileAs: register("onSaveFileAs"),
+  onRename: register("onRename"),
+  onImport: register("onImport"),
+  onExportWav: register("onExportWav"),
+  onUndo: register("onUndo"),
+  onRedo: register("onRedo"),
+  onCut: register("onCut"),
+  onCopy: register("onCopy"),
+  onPaste: register("onPaste"),
+  onOpenSetting: register("onOpenSetting"),
+  onOpenHelp: register("onOpenHelp"),
+  onBrowserSignInCompleted: register("onBrowserSignInCompleted"),
   // tell to main process that the renderer process is ready
-  ready: () => invoke("ready"),
-  showOpenDialog: async () => await invoke("showOpenDialog"),
-  showOpenDirectoryDialog: async () => await invoke("showOpenDirectoryDialog"),
-  showSaveDialog: async () => await invoke("showSaveDialog"),
-  saveFile: async (path, data) => await invoke("saveFile", path, data),
-  readFile: async (path) => await invoke("readFile", path),
-  searchSoundFonts: async (path) => await invoke("searchSoundFonts", path),
-  addRecentDocument: (path) => invoke("addRecentDocument", path),
-  getArgument: () => invoke("getArgument"),
-  openAuthWindow: async () => await invoke("openAuthWindow"),
-  authStateChanged: (isLoggedIn) => invoke("authStateChanged", isLoggedIn),
-  closeMainWindow: () => invoke("closeMainWindow"),
+  ready: invoke("ready"),
+  showOpenDialog: invoke("showOpenDialog"),
+  showOpenDirectoryDialog: invoke("showOpenDirectoryDialog"),
+  showSaveDialog: invoke("showSaveDialog"),
+  saveFile: invoke("saveFile"),
+  readFile: invoke("readFile"),
+  searchSoundFonts: invoke("searchSoundFonts"),
+  addRecentDocument: invoke("addRecentDocument"),
+  getArgument: invoke("getArgument"),
+  openAuthWindow: invoke("openAuthWindow"),
+  authStateChanged: invoke("authStateChanged"),
+  closeMainWindow: invoke("closeMainWindow"),
 }
 
 contextBridge.exposeInMainWorld("electronAPI", api)
