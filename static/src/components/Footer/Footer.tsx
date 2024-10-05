2336fceab4d693c9a584f93088d799f22293c290
ryohey <info@codingcafe.jp>
2024-10-05 11:42:51 +0900

    Fix layout

--- static/src/components/Footer/Footer.tsx
+++ static/src/components/Footer/Footer.tsx
@@ -2,6 +2,8 @@ import { styled } from "@kuma-ui/core"
 import Localized from "../Localized"
 
 const Content = styled.div`
+  display: flex;
+  gap: 1rem;
   padding: 2rem 0;
   display: flex;
   justify-content: center;
@@ -12,16 +14,23 @@ const Content = styled.div`
 `
 
 const Link = styled.a`
-  font-size: 0.9rem;
+  font-size: 0.8rem;
   color: white;
-  opacity: 0.6;
+  opacity: 0.5;
   text-decoration: none;
+
+  &:hover {
+    opacity: 1;
+  }
 `
 
 export const Footer = () => {
   return (
     <footer>
       <Content>
+        <Link href="/support">
+          <Localized name="support" />
+        </Link>
         <Link href="/privacy">
           <Localized name="privacy-policy-title" />
         </Link>
