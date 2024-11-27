b40f7b20d2a5c9010f822320a229da0d529b5aeb
ryohey <info@codingcafe.jp>
2024-11-27 15:14:20 +0900

    Add PianoRollAutoScrollButton

--- app/src/components/PianoRollToolbar/PianoRollToolbar.tsx
+++ app/src/components/PianoRollToolbar/PianoRollToolbar.tsx
@@ -1,14 +1,14 @@
 import styled from "@emotion/styled"
 import { observer } from "mobx-react-lite"
-import { FC, useCallback } from "react"
+import { FC } from "react"
 import { useStores } from "../../hooks/useStores"
 import InstrumentBrowser from "../InstrumentBrowser/InstrumentBrowser"
-import { AutoScrollButton } from "../Toolbar/AutoScrollButton"
 import { Toolbar } from "../Toolbar/Toolbar"
 import { TrackListMenuButton } from "../TrackList/TrackListMenuButton"
 import { EventListButton } from "./EventListButton"
 import { InstrumentButton } from "./InstrumentButton"
 import { PanSlider } from "./PanSlider"
+import { PianoRollAutoScrollButton } from "./PianoRollAutoScrollButton"
 import { PianoRollQuantizeSelector } from "./PianoRollQuantizeSelector"
 import { PianoRollToolSelector } from "./PianoRollToolSelector"
 import { TrackNameInput } from "./TrackNameInput"
@@ -25,16 +25,7 @@ const FlexibleSpacer = styled.div`
 export const PianoRollToolbar: FC = observer(() => {
   const { pianoRollStore } = useStores()
 
-  const { autoScroll, selectedTrack, selectedTrackId } = pianoRollStore
-
-  const onClickAutoScroll = useCallback(
-    () => (pianoRollStore.autoScroll = !pianoRollStore.autoScroll),
-    [pianoRollStore],
-  )
-
-  if (selectedTrack === undefined) {
-    return <></>
-  }
+  const { selectedTrackId } = pianoRollStore
 
   return (
     <Toolbar>
@@ -58,7 +49,7 @@ export const PianoRollToolbar: FC = observer(() => {
 
       <PianoRollQuantizeSelector />
 
-      <AutoScrollButton onClick={onClickAutoScroll} selected={autoScroll} />
+      <PianoRollAutoScrollButton />
     </Toolbar>
   )
 })
